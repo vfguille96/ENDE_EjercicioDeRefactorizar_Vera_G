96d59515d04d28a9460c769a184f8aa04a741d00
Guillermo Vera <vfguille@gmail.com>
2018-02-22 14:44:33 +0200

    Commit inicial

---
+++ ENDE_EjercicioDeRefactorizar_Vera_G/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactorizar
+{
+	public class Empleado: Persona {
+public string str;
+public int edad;
+public string numTelefono;
+public List<string> moviles;
+public Empleado (string telefono):base(telefono) { moviles.Add("<desconocido>");
+}
+public void ImprimeInfoPersonal () {
+Console.WriteLine("Nombre: {0}, Edad: {1}, Telefono: {2}", str, edad, numTelefono);
+}
+public void ImprimeTodo () { 
+	Console.WriteLine ("Nombre: {0}, Edad: {1}, Telefono: {2}", str, edad, numTelefono);
+Console.WriteLine ("Telefonos moviles: ");
+foreach (string s in moviles) {
+Console.WriteLine("Movil: {0}",s);
+}
+}
+}
+}
+}
---
+++ ENDE_EjercicioDeRefactorizar_Vera_G/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Refactorizar
+{
+ public class Persona {
+ public string numeroDeTelefono;
+ public Persona() {
+ Console.WriteLine("Error: este constructor " +
+"no deberia ser llamado");
+}
+public Persona (string telefono) {
+ this.numeroDeTelefono = telefono;
+}
+public string getNumeroDeTelefono () {
+ return this.numeroDeTelefono;
+}
+public void setNumeroDeTelefono (string numero) {
+ this.numeroDeTelefono = numero;
+}
+}
